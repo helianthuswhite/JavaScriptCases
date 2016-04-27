300f5e67024c9263cbeef7073129fb12724287bb
Littlewhite <964873559@qq.com>
2016-04-27 19:32:11 +0800

    Array

--- JavaScriptCases.js
+++ JavaScriptCases.js
@@ -211,3 +211,55 @@ function _18 () {
 	circ.setAttribute("cx",x);
 	circ.setAttribute("cy",y);
 }
+
+//创建多维数组
+function _19 () {
+	var arrayLength = 3;
+
+	//创建数组
+	var multiArray = new Array(arrayLength);
+	for(var i = 0;i < multiArray.length;i++){
+		multiArray[i] = new Array(arrayLength);
+	}
+
+	//给第一个数组索引添加项
+	multiArray[0][0] = "apple";
+	multiArray[0][1] = "banana";
+	multiArray[0][2] = "cherry";
+
+	//给第二个数组索引添加项
+	multiArray[1][0] = "2";
+	multiArray[1][1] = "56";
+	multiArray[1][2] = "83";
+
+	//给第三个数组索引项添加
+	multiArray[2][0] = ['test','again'];
+	multiArray[2][1] = ['java','script'];
+	multiArray[2][2] = ['read','books'];
+
+	alert(multiArray); //打印出所有数组的内容
+	alert(multiArray[2]); //打印出子数组
+	alert(multiArray[2][2][0]); //单个项目
+}
+
+//排序数组
+function _20 () {
+	var numberArray = [4,13,2,31,5];
+	alert(numberArray.sort()); //返回13,2,31,4,5
+
+	function compareNumbers (a,b) {
+		return a-b;
+	}
+	var numArray = [13,2,31,4,5];
+	alert(numArray.sort(compareNumbers));  //打印出2,4,5,13,31
+}
+
+//push()、pop()、unshift()、shift()方法
+function _21 () {
+	var stack = []; //stack:[]
+	stack.push(1); //stack:[1] 
+	stack.push(2); //stack:[1,2]
+	stack.pop(); //stack:[1] return 2;
+	stack.push([4,5]); //stack:[1,[4,5]]
+	stack.pop();  //stack:[1] return [4,5]
+}
