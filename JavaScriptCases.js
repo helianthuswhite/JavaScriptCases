6a0185eda53f101b940094a9863d2573e8f1a8f3
Littlewhite <964873559@qq.com>
2016-06-13 14:18:11 +0800

    获取鼠标点击事件

--- JavaScriptCases.js
+++ JavaScriptCases.js
@@ -503,4 +503,33 @@ function _32 () {
 	//缓存计算：160ms
 }
 
-//
+//使用Event对象捕获鼠标点击事件的位置
+function _33 () {
+	document.onclick = processClick;
+
+	function processClick (evt) {
+		//访问事件对象
+		evt = evt || window.event;
+		var x = 0,y = 0;
+
+		//如果事件对象有pageX,pageY属性
+		if (evt.pageX&&evt.pageY) {
+			x = evt.pageX;
+			y = evt.pageY;
+		} else if (evt.clientX&&evt.clientY) {
+			var offsetX = 0,offsetY = 0;
+			//IE6以上版本
+			if(document.doucmentElement.scrollLeft) {
+				offsetX = document.doucmentElement.scrollLeft;
+				offsetY = doucment.doucmentElement.scrollTop;
+			//IE6以前的版本
+			} else if(doucment.body) {
+				offsetX = doucment.body.scrollLeft;
+				offsetY = doucment.body.scrollTop;
+			}
+			x = evt.clientX + offsetX;
+			y = evt.clientY + offsetY;
+		}
+		alert("you clicked aata x=" + x + " y=" + y);
+	}
+}
