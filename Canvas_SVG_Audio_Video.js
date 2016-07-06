0174078cb59ac7777ee99dd4e08ee92befe6da2d
Littlewhite <964873559@qq.com>
2016-07-06 23:23:31 +0800

    canvas

--- Canvas_SVG_Audio_Video.js
+++ Canvas_SVG_Audio_Video.js
@@ -5,7 +5,13 @@
  */
 
 
-//
+//向页面中插入一个canvas
 function _52() {
-	// body...
+	var imgcanvas = document.getElementById('imgcanvas');
+	if (imgcanvas.getContext) {
+		var ctx = imgcanvas.getContext('2d');
+		ctx.fillStyle = '#000';
+		ctx.fillRect(0,0,100,100);
+		ctx.strokeRect(0,0,100,100);
+	}
 }
